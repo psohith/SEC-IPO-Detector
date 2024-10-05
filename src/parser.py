a153f521a3a1cdfd3c78c1628a4546a374d70266
sohithPamu <83358184+psohith@users.noreply.github.com>
2024-10-06 01:30:40 +0530

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -1,21 +1,32 @@
 # parser.py
 import xml.etree.ElementTree as ET
+from datetime import datetime
 from .constants import IPO_KEYWORDS
 from .fetcher import fetch_form_s1_content
 
-def parse_rss_feed(content):
-    """Parse the RSS XML content and return a list of entries."""
+
+
+def parse_rss_feed(content, latest_updated_time=None):
+    """Parse the RSS XML content and return a list of entries up to the latest_updated_time"""
     ns = {'atom': 'http://www.w3.org/2005/Atom'}
     entries = []
     root = ET.fromstring(content)
+
     for entry in root.findall('atom:entry', ns):
+        updated_time_str = entry.find('atom:updated', ns).text
+        updated_time = datetime.strptime(updated_time_str,  '%Y-%m-%dT%H:%M:%S%z')
+
+        if latest_updated_time is not None and updated_time <= latest_updated_time:
+            break
+            
         entries.append({
             'title': entry.find('atom:title', ns).text,
             'url': entry.find('atom:link', ns).attrib['href'],
             'summary': entry.find('atom:summary', ns).text.strip(),
-            'updated': entry.find('atom:updated', ns).text,
+            'updated': updated_time_str,
             'category': entry.find('atom:category', ns).attrib.get('term'),
         })
+
     return entries
 
 
