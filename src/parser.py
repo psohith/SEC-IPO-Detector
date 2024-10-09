ab1d37daeef94a62adc63b34141082184c6323fe
sohithPamu <83358184+psohith@users.noreply.github.com>
2024-10-09 23:30:41 +0530

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -3,11 +3,13 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 from .constants import IPO_KEYWORDS
 from .fetcher import fetch_form_s1_content
+from .utils import normalize_spaces_and_newlines
+from .openAI import get_gpt_responce
 
 
 
 def parse_rss_feed(content, latest_updated_time=None):
-    """Parse the RSS XML content and return a list of entries up to the latest_updated_time"""
+    """Parse the RSS XML content and return a list of entries up to the  latest_updated_time"""
     ns = {'atom': 'http://www.w3.org/2005/Atom'}
     entries = []
     root = ET.fromstring(content)
@@ -18,7 +20,7 @@ def parse_rss_feed(content, latest_updated_time=None):
 
         if latest_updated_time is not None and updated_time <= latest_updated_time:
             break
-            
+
         entries.append({
             'title': entry.find('atom:title', ns).text,
             'url': entry.find('atom:link', ns).attrib['href'],
@@ -33,4 +35,13 @@ def parse_rss_feed(content, latest_updated_time=None):
 def is_ipo_filing(filing_url):
     """Check if the document content is related to an IPO."""
     document_content = fetch_form_s1_content(filing_url)
-    return any(keyword in document_content for keyword in IPO_KEYWORDS)
+    document_content = normalize_spaces_and_newlines(document_content)
+    index_table_ind = 12000
+    try:
+        index_table_ind = document_content.index("table of content")
+    except:
+        pass
+    trimed_doc = document_content[:index_table_ind]
+    gptResonce = str(get_gpt_responce(trimed_doc)).lower()
+    keywords = ['yes','true']
+    return any(keyword in gptResonce for keyword in keywords)
